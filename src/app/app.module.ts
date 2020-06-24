066cc029ed6e5ef2bae5f829c76fe94199230db6
ejmordo <aka.drozdie@gmail.com>
2020-06-24 22:53:37 +0200

    Added tic-tac-toe, routing, rps, update bf snake

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,45 @@
-import { NgModule } from '@angular/core';
+import { NgModule, DoBootstrap, Injector } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BoardComponent } from './board/board.component';
+import { SquareComponent } from './square/square.component';
+import { RpsComponent } from './rps/rps.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule, NbButtonModule, NbIconModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { SnakeComponent } from './snake/snake.component';
+import {createCustomElement} from '@angular/elements';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent, 
+    HomeComponent,
+    BoardComponent, 
+    SquareComponent, 
+    RpsComponent, 
+    SnakeComponent
+  ],
+  imports: [
+    BrowserModule, 
+    AppRoutingModule, 
+    BrowserAnimationsModule, 
+    NbThemeModule.forRoot({ name: 'dark' }), 
+    NbLayoutModule, 
+    NbEvaIconsModule,
+    NbButtonModule,
+    NbIconModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+
+export class AppModule implements DoBootstrap{
+  constructor(injector: Injector){
+    const snakeComponent = createCustomElement(SnakeComponent, {injector});
+    customElements.define('ng-snake', snakeComponent);
+  }
+  ngDoBootstrap(appRef: import("@angular/core").ApplicationRef): void { }
+  
+}
