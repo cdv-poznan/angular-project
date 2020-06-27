2d2818424cc6dc74499934460e9cd01ec46d7e79
KrystianDerdzinski <derdzinski.krystian@gmail.com>
2020-06-27 22:15:15 +0200

    Color picker display

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,21 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { ConfiguratorTshirtComponent } from './configurator-tshirt/configurator-tshirt.component';
+import { fabric } from 'fabric';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Form, FormsModule } from '@angular/forms';
+import { MatRadioModule, MAT_RADIO_DEFAULT_OPTIONS } from '@angular/material/radio';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, LandingPageComponent, ConfiguratorTshirtComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, FormsModule, BrowserAnimationsModule, MatRadioModule],
+  providers: [
+    {
+      provide: MAT_RADIO_DEFAULT_OPTIONS,
+      useValue: { color: 'accent' },
+    }
+  ],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/configurator-tshirt/configurator-tshirt.component.ts
+++ src/app/configurator-tshirt/configurator-tshirt.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { fabric } from 'fabric';
+import { Form } from '@angular/forms';
+import { MatRadioModule } from '@angular/material/radio';
 
 @Component({
   selector: 'app-configurator-tshirt',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./configurator-tshirt.component.scss']
 })
 export class ConfiguratorTshirtComponent implements OnInit {
-
+  canvas: any;
+  selectedColor = 'white';
   constructor() { }
 
   ngOnInit(): void {
+    this.canvas = new fabric.Canvas('tshirtCanvas');
+    // this.canvas.add(new fabric.IText('Hello Fabric!'));
+    fabric.Image.fromURL('../assets/img/tshirt.png', img => {this.canvas.add(img)});
   }
+
+
+  public onColorSelection(value) {
+    // this.selectedColor =
+    console.log(value);
+  }
+
+
 
 }
