b34a6a3db030a919420719a5dd6b7cf9fde7d37c
Gerard Urbański <biuro@internetprojects.pl>
2020-07-03 01:29:37 +0200

    start - generowanie komponentów podstawowych

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TeamsComponent } from './teams/teams.component';
+import { PlayersComponent } from './players/players.component';
+import { GamesComponent } from './games/games.component';
+import { SharedComponent } from './shared/shared.component';
+import { TeamsListComponent } from './teams/teams-list/teams-list.component';
+import { TeamDetailsComponent } from './teams/team-details/team-details.component';
+import { PlayersListComponent } from './players/players-list/players-list.component';
+import { PlayerDetailsComponent } from './players/player-details/player-details.component';
+import { GamesListComponent } from './games/games-list/games-list.component';
+import { GameDetailsComponent } from './games/game-details/game-details.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TeamsComponent, PlayersComponent, GamesComponent, SharedComponent, TeamsListComponent, TeamDetailsComponent, PlayersListComponent, PlayerDetailsComponent, GamesListComponent, GameDetailsComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/players/player-details/player-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-player-detail',
+  templateUrl: './player-detail.component.html',
+  styleUrls: ['./player-detail.component.scss']
+})
+export class PlayerDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
