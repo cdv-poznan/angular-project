5954bfe87245d633a1036a3cd6cf552f2783c6cd
Matkup <matkup@192.168.1.7>
2020-07-04 18:07:46 +0200

    first update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherItemComponent } from './weather-item/weather-item.component';
+import { ItemListItemComponent } from './item-list-item/item-list-item.component';
+import { PlanItemComponent } from './plan-item/plan-item.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, WeatherItemComponent, ItemListItemComponent, PlanItemComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/plan-item/plan-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-plan-item',
+  templateUrl: './plan-item.component.html',
+  styleUrls: ['./plan-item.component.scss']
+})
+export class PlanItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
